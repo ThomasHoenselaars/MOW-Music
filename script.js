605156baa500d1ec367cd9d76d707095315eff9b
Thomas Hoenselaars <heemsteda@gmail.com>
2019-03-05 19:31:08 +0100

    flex update en js doodles

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-const animateHTML = () => {
-    let elems;
-    let windowHeight;
+// const animateHTML = () => {
+//     let elems;
+//     let windowHeight;
     
-    const init = () => {
-        elems = document.querySelectorAll('.hidden');
-        windowHeight = window.innerHeight;
-    }
+//     const init = () => {
+//         elems = document.querySelectorAll('.hidden');
+//         windowHeight = window.innerHeight;
+//     }
 
-    const checkPosition = () => {
-        for (var i = 0; i < elems.length; i++) {
-            var positionFromTop = elems[i].getBoundingClientRect().top;
-            if (positionFromTop - windowHeight <= 0) {
-                elems[i].className = elems[i].className.replace(
-                    'hidden',
-                    'fade-in-element'
-                );
-            }
-        }
-    }
+//     const checkPosition = () => {
+//         for (var i = 0; i < elems.length; i++) {
+//             var positionFromTop = elems[i].getBoundingClientRect().top;
+//             if (positionFromTop - windowHeight <= 0) {
+//                 elems[i].className = elems[i].className.replace(
+//                     'hidden',
+//                     'fade-in-element'
+//                 );
+//             }
+//         }
+//     }
 
-    window.addEventListener('click', checkPosition);
+//     window.addEventListener('click', checkPosition);
 
-    return {
-        init: init
-    };
+//     return {
+//         init: init
+//     };
 
     
-};
+// };
 
 
-animateHTML().init();
+// animateHTML().init();
+
+// const slideOne = document.querySelector('#slide1');
+// const navLinks = document.querySelectorAll('.navlist');
+// console.log(navLinks);
+
+// const slideToView = () => {
+//     slideOne.scrollIntoView({ behavior: 'smooth' });
+// };
+
+// navLinks.forEach(navLink => {
+//     console.log(navLink);
+// });
+
+// const closeMenu = document.querySelector('.closeframe');
+// const openMenu = document.querySelector('.openframe');
+// const bookingPopup = document.querySelector('.booking-popup');
+
+
+// openMenu.addEventListener('click', () => {
+//     bookingPopup.classList.add('visible');
+// });
+
+// closeMenu.addEventListener('click', () => {
+//     bookingPopup.classList.remove('visible');
+// });
